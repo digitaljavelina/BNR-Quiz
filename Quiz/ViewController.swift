094ab78c06a00a47ff8ae7da2123caf7d4dd09f6
Michael Henry <mike@digitaljavelina.com>
2016-06-18 23:10:43 -0700

    added animations and layoutIfNeeded()

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -10,28 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var questionLabel: UILabel!
-    @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet var currentQuestionLabel: UILabel!
+    @IBOutlet var currentQuestionLabelCenterXConstraint: NSLayoutConstraint!
+    @IBOutlet var nextQuestionLabel: UILabel!
+    @IBOutlet var nextQuestionLabelCenterXConstraint: NSLayoutConstraint!
+    @IBOutlet var answerLabel: UILabel!
     
-    let questionArray: [String] = ["What is 2 + 2?", "When will Then be Now?", "What is the average flight speed of a swallow?"]
+    let questions: [String] = ["What is 2 + 2?", "When will Then be Now?", "What is the average flight speed of a swallow?"]
     let answerArray: [String] = ["4", "Soon", "African or European?"]
-    var currentQuestionIndex: Int = -1    // so that when currentQuestionIndex +=1 occurs, the first item called will [0]
+    var currentQuestionIndex: Int = 0
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // labels are blank to start
-        questionLabel.text = ""
+        
+        let question = questions[currentQuestionIndex]
+        currentQuestionLabel.text = question
         answerLabel.text = ""
+        
+        updateOffScreenLabel()
+    }
+    
+    func updateOffScreenLabel() {
+        let screenwidth = view.frame.width
+        nextQuestionLabelCenterXConstraint.constant = -screenwidth
     }
 
     @IBAction func handleAskQuestionButton(sender: UIButton) {
         currentQuestionIndex += 1
         // reset currentQuestionIndex back to 0 if at the last item in the array
-        if currentQuestionIndex == questionArray.count {
+        if currentQuestionIndex == questions.count {
             currentQuestionIndex = 0
-    }
-        questionLabel.text = questionArray[currentQuestionIndex]
+        }
+        let question: String = questions[currentQuestionIndex]
+        nextQuestionLabel.text = question
+        
+        // call the animation
+        animateLabelTransitions()
         
     }
     
@@ -41,7 +56,34 @@ class ViewController: UIViewController {
         }
     
   
+    func animateLabelTransitions() {
+        // force any outstanding layout changes to occur
+        view.layoutIfNeeded()
+        
+        // animate the alpha and the centerX constraints
+        let screenwidth = view.frame.width
+        self.nextQuestionLabelCenterXConstraint.constant = 0
+        self.currentQuestionLabelCenterXConstraint.constant += screenwidth
+        
+        UIView.animateWithDuration(0.5, delay: 0, options: [.CurveLinear], animations: {
+            self.currentQuestionLabel.alpha = 0
+            self.nextQuestionLabel.alpha = 1
+            self.view.layoutIfNeeded()    // update views based on new constraints
+            }, completion: { _ in
+                swap(&self.currentQuestionLabel, &self.nextQuestionLabel)
+                swap(&self.currentQuestionLabelCenterXConstraint, &self.nextQuestionLabelCenterXConstraint)
+                
+                self.updateOffScreenLabel()
+        })
+    }
     
-
+    // reset the alpha to 0 just before the view appears
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // set the label's initial alpha
+        nextQuestionLabel.alpha = 0
+    }
+    
 }
 

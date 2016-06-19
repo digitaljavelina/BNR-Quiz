b90f035f821d5ee5bb5b7aadb89f7270490f5ff7
Michael Henry <mike@digitaljavelina.com>
2016-06-18 20:51:20 -0700

    added documentation to code in initial commit

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -15,17 +15,19 @@ class ViewController: UIViewController {
     
     let questionArray: [String] = ["What is 2 + 2?", "When will Then be Now?", "What is the average flight speed of a swallow?"]
     let answerArray: [String] = ["4", "Soon", "African or European?"]
-    var currentQuestionIndex: Int = -1
+    var currentQuestionIndex: Int = -1    // so that when currentQuestionIndex +=1 occurs, the first item called will [0]
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        // labels are blank to start
         questionLabel.text = ""
         answerLabel.text = ""
     }
 
     @IBAction func handleAskQuestionButton(sender: UIButton) {
         currentQuestionIndex += 1
+        // reset currentQuestionIndex back to 0 if at the last item in the array
         if currentQuestionIndex == questionArray.count {
             currentQuestionIndex = 0
     }
@@ -34,6 +36,7 @@ class ViewController: UIViewController {
     }
     
     @IBAction func handleAnswerButton(sender: UIButton) {
+        // keep questionArray and answerArray in sync
         answerLabel.text = answerArray[currentQuestionIndex]
         }
     
